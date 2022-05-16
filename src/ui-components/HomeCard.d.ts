41a81f8b688bb3b055f640119e478ee7d59be8ac
sa-asamizu <sa-asamizu@secom.co.jp>
2022-05-16 16:04:47 +0900

    所在をテキストフィールドへ変更

--- src/ui-components/HomeCard.d.ts
+++ src/ui-components/HomeCard.d.ts
@@ -5,8 +5,8 @@
  **************************************************************************/
 
 import React from "react";
-import { EscapeHatchProps } from "@aws-amplify/ui-react/internal";
 import { Home } from "../models";
+import { EscapeHatchProps } from "@aws-amplify/ui-react/internal";
 import { FlexProps } from "@aws-amplify/ui-react";
 export declare type HomeCardProps = React.PropsWithChildren<Partial<FlexProps> & {
     home?: Home;
--- src/ui-components/HomeCard.jsx
+++ src/ui-components/HomeCard.jsx
@@ -6,21 +6,10 @@
 
 /* eslint-disable */
 import React from "react";
-import {
-  getOverrideProps,
-  useNavigateAction,
-} from "@aws-amplify/ui-react/internal";
-import { Flex, Text, View } from "@aws-amplify/ui-react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Flex, Text, TextField } from "@aws-amplify/ui-react";
 export default function HomeCard(props) {
   const { home, overrides, ...rest } = props;
-  const rectangleOneOneSixFourOnClick = useNavigateAction({
-    type: "url",
-    url: home?.status,
-  });
-  const tELEWORKOnClick = useNavigateAction({
-    type: "url",
-    url: home?.location,
-  });
   return (
     <Flex
       gap="24px"
@@ -35,58 +24,6 @@ export default function HomeCard(props) {
       {...rest}
       {...getOverrideProps(overrides, "HomeCard")}
     >
-      <View
-        width="160px"
-        height="43px"
-        shrink="0"
-        position="relative"
-        padding="0px 0px 0px 0px"
-        {...getOverrideProps(overrides, "Frame 418")}
-      >
-        <View
-          width="160px"
-          height="43px"
-          position="absolute"
-          top="0px"
-          left="0px"
-          borderRadius="10px"
-          padding="0px 0px 0px 0px"
-          backgroundColor="rgba(45,103,160,1)"
-          onClick={() => {
-            rectangleOneOneSixFourOnClick();
-          }}
-          {...getOverrideProps(overrides, "Rectangle 1164")}
-        ></View>
-        <Flex
-          gap="16px"
-          position="absolute"
-          top="10px"
-          left="34px"
-          direction="column"
-          alignItems="center"
-          padding="0px 0px 0px 0px"
-          {...getOverrideProps(overrides, "status")}
-        >
-          <Text
-            fontFamily="Inter"
-            fontSize="24px"
-            fontWeight="700"
-            color="rgba(255,255,255,1)"
-            lineHeight="24px"
-            textAlign="center"
-            display="flex"
-            direction="column"
-            justifyContent="flex-start"
-            letterSpacing="0.09px"
-            shrink="0"
-            position="relative"
-            padding="0px 0px 0px 0px"
-            whiteSpace="pre-wrap"
-            children="ONLINE"
-            {...getOverrideProps(overrides, "ONLINE")}
-          ></Text>
-        </Flex>
-      </View>
       <Flex
         gap="8px"
         direction="column"
@@ -114,37 +51,20 @@ export default function HomeCard(props) {
           {...getOverrideProps(overrides, "Melinda Marcus")}
         ></Text>
       </Flex>
-      <Flex
-        gap="16px"
+      <TextField
+        display="flex"
         direction="column"
-        alignItems="center"
+        width="300px"
         shrink="0"
         position="relative"
         padding="0px 0px 0px 0px"
-        {...getOverrideProps(overrides, "location")}
-      >
-        <Text
-          fontFamily="Inter"
-          fontSize="20px"
-          fontWeight="400"
-          color="rgba(35,47,62,1)"
-          lineHeight="24px"
-          textAlign="center"
-          display="flex"
-          direction="column"
-          justifyContent="flex-start"
-          letterSpacing="0.05px"
-          shrink="0"
-          position="relative"
-          padding="0px 0px 0px 0px"
-          whiteSpace="pre-wrap"
-          children="TELEWORK"
-          onClick={() => {
-            tELEWORKOnClick();
-          }}
-          {...getOverrideProps(overrides, "TELEWORK")}
-        ></Text>
-      </Flex>
+        placeholder="location"
+        size="large"
+        isDisabled={false}
+        labelHidden={true}
+        variation="quiet"
+        {...getOverrideProps(overrides, "TextField")}
+      ></TextField>
     </Flex>
   );
 }
